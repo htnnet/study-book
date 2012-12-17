55b73c03ad8ccb20478536ef6cfe06c0de3752a6
a.k3ll3r@gmail.com <a.k3ll3r@gmail.com@1070db7b-cc67-0279-0add-421bf1dd945f>
2012-12-17 18:24:25 +0000

    + das HilfsPanel ist vorbereitet, es muss lediglich die Datei help.html mit den Angaben aus help.txt gefüllt und ergänzt werden - der Über-Dialog folgt noch - Kommentare auch

--- trunk/StudyBook/src/studybook/SBHelpPanel.java
+++ trunk/StudyBook/src/studybook/SBHelpPanel.java
@@ -1,6 +1,7 @@
 package studybook;
 
 import java.awt.BorderLayout;
+import java.io.IOException;
 import javax.swing.border.Border;
 import javax.swing.*;
 
@@ -12,20 +13,40 @@ import javax.swing.*;
  * @since 2012-10-14
  */
 public class SBHelpPanel extends JPanel {
+
     private JLabel hilfe;
+    private JEditorPane editorPane;
+    private JScrollPane editorScrollPane;
 
     public SBHelpPanel() {
         this.createHelpPanel();
         this.layoutHelpPanel();
     }
 
-
     /**
-    * Erstellt das Hilfspanel, das dem Benutzer das Programm näher bringen soll.
-    */
+     * Erstellt das Hilfspanel, das dem Benutzer das Programm näher bringen
+     * soll.
+     */
     public void createHelpPanel() {
         hilfe = new JLabel("Hilfe!!!");
-        hilfe.setHorizontalAlignment(SwingConstants.CENTER);
+        this.createEditorPane();
+        editorScrollPane = new JScrollPane(this.editorPane);
+        editorScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+    }
+
+    private void createEditorPane() {
+        editorPane = new JEditorPane();
+        editorPane.setEditable(false);
+        java.net.URL helpURL = this.getClass().getResource("/doc/help.html");
+        if (helpURL != null) {
+            try {
+                editorPane.setPage(helpURL);
+            } catch (IOException exception) {
+                System.err.println("Konnte URL nicht finden " + helpURL);
+            }
+        } else {
+            System.err.println("Die Datei \"help.html\" konnte nicht gefunden werden");
+        }
     }
 
     /**
@@ -33,6 +54,6 @@ public class SBHelpPanel extends JPanel {
      */
     public void layoutHelpPanel() {
         this.setLayout(new BorderLayout());
-        this.add(hilfe, BorderLayout.CENTER);
+        this.add(editorScrollPane, BorderLayout.CENTER);
     }
 }
